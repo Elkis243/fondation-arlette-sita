95a69d265c51601f2918584e43cdeca913c2ecc2
Elkis243 <elielubama@gmail.com>
2025-03-27 21:51:51 +0100

    feat: ajouter une page d'erreur 404 personnalisée avec un design et des métadonnées appropriés

--- fas/settings/base.py
+++ fas/settings/base.py
@@ -15,18 +15,18 @@ from dotenv import load_dotenv
 import os
 from django.contrib import messages
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
-
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 # Application definition
 
